c9db55087ef902b51b35e7a75a227ed0b8e19adc
francescocastelli <francesco8.castelli@mail.polimi.it>
2021-12-21 16:11:19 +0100

    fixed conda build options

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-setup(name='genparser',
+setup(name='gen-parser',
       version='0.1',
       packages=find_packages(),
       include_package_data=True,
